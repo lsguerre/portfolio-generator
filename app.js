89ae57cdd597566f18d3927df405effa9a3b6dc7
Opal <guerre5lily@gmail.com>
2021-12-02 23:01:15 -0800

    add inquirer

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer')
+// const fs = require('fs')
+// const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2)
-const [name, github] = profileDataArgs
+// const pageHTML = generatePage(name, github)
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err
+//   console.log('Portfolio complete! Check out index.html to see the output!')
+// })
 
-  console.log('Portfolio complete! Check out index.html to see the output!')
-})
