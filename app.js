eea5119027f22eac1ab4138eb04cff3d119e7948
Opal <guerre5lily@gmail.com>
2021-12-04 18:08:42 -0800

    ask name

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ const inquirer = require('inquirer')
 //   console.log('Portfolio complete! Check out index.html to see the output!')
 // })
 
+inquirer 
+    .prompt([
+        //questoins here
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+
+    ])
+    .then(answers => console.log(answers))
