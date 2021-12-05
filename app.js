a2c6f146cc5c5e8efd22366c3cb64114e0c911e3
Opal <guerre5lily@gmail.com>
2021-12-04 22:20:16 -0800

    prompt user and store data

--- app.js
+++ app.js
@@ -11,13 +11,141 @@ const inquirer = require('inquirer')
 // })
 
 inquirer 
-    .prompt([
-        //questoins here
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    }
-
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter your name!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter your GitHub username!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself',
+            when: ({ confirmAbout }) => {
+                if(confirmAbout) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
     ])
-    .then(answers => console.log(answers))
+}
+    
+    
+
+const promptProject = portfolioData => {
+   console.log(`
+    =================
+    Add a New Project
+    =================
+    `)
+
+    //only initialize on first pass
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+      }    
+    
+    return inquirer
+     .prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the name of your project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('You need to enter a project description!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the link to your GitHub project!')
+                    return false
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData)
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData)
+        } else {
+            return portfolioData
+        }
+    })
+}
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData)
+})
